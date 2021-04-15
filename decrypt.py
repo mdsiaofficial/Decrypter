81987952912e77e9e39573765c46eba2558a1ca3
Md Shoriful Islam <mdsiaofficial@gmail.com>
2021-04-15 23:18:35 +0600

    Create decrypt.py

---
+++ decrypt.py
@@ -0,0 +1,14 @@
+print("==== Cesar Decrypt ====")
+
+alphabet = []
+alphabet =  ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',' ','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+alphabettrois =  ['d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','a','b','c',' ','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','A','B','C']
+message = ''
+compteur_alphabet = len(alphabet)
+
+chaine = input(">>> ")
+
+for lettre in chaine:
+    message += alphabet[alphabettrois.index(lettre)]
+
+print("Decrypted Message = ",message)
